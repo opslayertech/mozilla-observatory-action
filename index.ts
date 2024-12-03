da0d9d46fc6affb056e4676681c325e2214f204b
Richard Kenigs <riha@opslayer.com>
2024-12-03 14:04:37 +0100

    try to run

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { scan } from "./src/index.ts";
 async function main() {
 	try {
 		const url = core.getInput("url");
+		console.log(`URL from Input: ${url}`);
 		await scan({ url });
 	} catch (error) {
 		core.setFailed(error.message);
